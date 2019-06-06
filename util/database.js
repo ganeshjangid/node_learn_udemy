f25a18eb574975dfaba2a57803ab9df7bff4ceb1
Ganesh <webdeveloper.ganesh@gmail.com>
2019-06-06 06:30:07 +0530

    test

--- util/database.js
+++ util/database.js
@@ -1,8 +1,12 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node_js_learn', 'application', 's@myD#@mnl@sy', {
+const sequelize = new Sequelize('node_js_udemy_myself', 'root', 'ganesh2468*', {
+  dialect: 'mysql',
+  host: '127.0.0.1'
+});
+/* const sequelize = new Sequelize('node_js_learn', 'application', 's@myD#@mnl@sy', {
   dialect: 'mysql',
   host: '172.29.67.213'
-});
+}); */
 
 module.exports = sequelize;
